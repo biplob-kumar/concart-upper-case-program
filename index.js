5ef3f0075a02abd1a7927589cca4981b78d2019e
biplob kumar <biplobkumar125@gmail.com>
2023-02-02 09:30:32 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,10 @@
+//  string concart program  start
+
+var x="biplob"
+var y="kumar"
+var z="mondol"
+var sum=x+y+z;
+  var upper =sum.toUpperCase()
+console.log(upper);
+
+//  string concart program  end 
